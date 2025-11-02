cb083ae4522e8dc7ddf03b2c997c3663aa23c82f
Monish-cloud8 <amonish2011@gmail.com>
2025-11-02 13:54:09 -0600

    Test:API URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
-
+const API_BASE_URL = 'process.env.REACT_APP_API_URL';
 // Signup API call
 export const signup = async (userData) => {
   try {
